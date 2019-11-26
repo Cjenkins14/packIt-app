92bab23bb59036b9b941b39932f6eef0130b71f0
Cjenkins14 <54726437+Cjenkins14@users.noreply.github.com>
2019-11-26 12:16:04 -0600

    "MVP wireframe"

---
+++ index.js
@@ -0,0 +1,46 @@
+// function to handle start click
+function handleStart() {
+
+}
+
+// function to handle submit
+function watchForm() {
+
+}
+
+// function to format query
+function formatQuery() {
+
+}
+
+// function to fetch from API
+function getData() {
+
+}
+
+// function to render results
+function displayResults() {
+
+}
+
+// function to handle item click
+function getItem() {
+
+}
+
+// function to fetch from API
+function getProduct() {
+
+}
+
+// function to render products
+function displayProducts() {
+
+}
+
+// reset button
+function nowReset() {
+
+}
+
+$(watchForm)
