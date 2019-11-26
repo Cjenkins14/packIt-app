047e933c0347234018562961d61ef082d82a2537
Cjenkins14 <54726437+Cjenkins14@users.noreply.github.com>
2019-11-26 13:27:25 -0600

    "added handleStart and watchForm, html fix"

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
+const baseUrl = 'https://api.weatherbit.io/v2.0/forecast/daily'
+const apiKey = '1f02fa7f33fd4eb9b8c87429b31880a1'
+
 // function to handle start click
 function handleStart() {
+    $('.start').on('click', function (event) {
+        $('.user-input').removeClass('hidden');
+        $('.home').addClass('hidden');
+        watchForm();
+    })
 
 }
 
 // function to handle submit
 function watchForm() {
+    $('form').submit(function (event) {
+        event.preventDefault();
+    })
+    let postCode = $('input[#js-zip]').val();
+    let startDate = $('input[#js-start-date]').val();
+    let endDate = $('input[#js-end-date]').val();
+    formatQuery(postCode);
+    dateFormat(startDate, endDate);
+};
 
-}
+// function to format dates
+function dateFormat(startDate, endDate) {
+    
+};
 
 // function to format query
-function formatQuery() {
-
+function formatQuery(params) {
+    
 }
 
 // function to fetch from API
-function getData() {
-
+function getData(postCode) {
+    
 }
 
 // function to render results
@@ -43,4 +63,4 @@ function nowReset() {
 
 }
 
-$(watchForm)
+$(handleStart)
