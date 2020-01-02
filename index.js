4387375cf54d281d3d3fb78ea1bd8464a406bd3f
Cjenkins14 <54726437+Cjenkins14@users.noreply.github.com>
2020-01-02 16:25:35 -0600

    "full revision, bug fixes"

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const BASE_URL = 'https://api.weatherbit.io/v2.0/forecast/daily'
 const APIKEY = '1f02fa7f33fd4eb9b8c87429b31880a1'
 let itemList = [];
+let date = new Date();
+let today = new Date(date.setHours(0, 0, 0, 0));
+const dateLimit = new Date(date.setDate(date.getDate() + 16));
+const tripData = {
+    start: null,
+    end: null,
+    postCode: null,
+    today: new Date()
+}
 
 
 // function to handle start click
@@ -9,26 +18,35 @@ function handleStart() {
         $('form').removeClass('hidden');
         $('#home').addClass('hidden');
     })
+}
 
+// function to handle date formats and update variables
+function dataHandler() {
+    tripData.postCode = $("input[name='zip-code']").val();
+    const startDate = $("input[name='start-date']").val();
+    const endDate = $("input[name='end-date']").val();
+    const startSplit = startDate.split("-");
+    const endSplit = endDate.split("-");
+    tripData.start = new Date(startSplit);
+    tripData.end = new Date(endSplit);
 }
 
 // function to handle submit
 function watchForm() {
-    $('.js-submit').on('click', (event => {
+    $('.js-submit').on('click', event => {
         event.preventDefault();
         resultReset();
-        let postCode = $("input[name='zip-code']").val();
-        let startDate = new Date($("input[name='start-date']").val());
-        let endDate = new Date($("input[name='end-date']").val());
-        if (endDate.getDate() - startDate.getDate() > 7) {
-            $('.js-error-msg').text('Sorry, please restrict input to 7 days');
+        dataHandler();
+        if (tripData.start < today || dateLimit < tripData.end) {
+            $('.js-error-msg').text('Please use dates from today, up to 16 days in the future');
+            $('.js-error-msg').removeClass('hidden');
+        } else if (tripData.end.getDate() - tripData.start.getDate() > 16) {
+            $('.js-error-msg').text('Please restrict the input to 2 weeks');
             $('.js-error-msg').removeClass('hidden');
         } else {
-            getData(postCode, startDate, endDate);
+            getData();
         }
-
-    }))
-
+    })
 };
 
 // function to reset results
@@ -45,48 +63,45 @@ function formatQuery(params) {
 }
 
 // function to fetch from API
-function getData(postCode, startDate, endDate) {
+function getData() {
     let params = {
         key: APIKEY,
-        postal_code: postCode,
+        postal_code: tripData.postCode,
         units: 'I'
     }
     let queryString = formatQuery(params)
     let url = (`${BASE_URL}?${queryString}`);
-    console.log(url);
 
     fetch(url)
         .then(response => {
-            if (response.ok && response.status !== 204) {
+            if (response.status == 204) {
+                throw new Error(response.statusText);
+            } else if (response.ok) {
                 return response.json();
             } else {
                 throw new Error(response.statusText);
             }
         })
-        .then(responseJson => checkDates(responseJson, startDate, endDate))
+        .then(responseJson => checkDates(responseJson))
         .catch(error => {
-            $('.js-error-msg').text(`Something went wrong: ${error.message}. Please try again later.`)
+            $('.js-error-msg').text(`Something went wrong: ${error.message} for postal code. Please try again later.`)
             $('.js-error-msg').removeClass('hidden')
         })
-
 };
 
 // function to check start and end date
-function checkDates(responseJson, startDate, endDate) {
+function checkDates(responseJson) {
 
     for (let i = 0; i < responseJson["data"].length; i++) {
         let thisDate = new Date(`${responseJson["data"][i]["datetime"]}`)
         let thisDay = new Date(thisDate.setHours(00));
-        if (thisDay >= startDate && thisDay <= endDate) {
+        if (thisDay >= tripData.start && thisDay <= tripData.end) {
             renderTemps(i, responseJson)
             let itemList = findGear(i, responseJson)
-        } else {
-            console.log('Days not within range')
         }
     }
     sortRenderGear(itemList);
     $('#results').removeClass('hidden');
-    nowClear();
 }
 
 // function to render temps
@@ -97,7 +112,6 @@ function renderTemps(i, responseJson) {
     $('.js-hi-lo').append(`<li>${tempHi}&#176;<br>
     ${tempLo}&#176;
     </li>`);
-
 }
 
 
@@ -107,7 +121,6 @@ function findGear(i, responseJson) {
     let tempLo = `${responseJson["data"][i]["min_temp"]}`
     let snowFall = `${responseJson["data"][i]["snow_depth"]}`
     let rainFall = `${responseJson["data"][i]["precip"]}`
-
 
     if (snowFall > 1) {
         (itemList).push(
@@ -145,25 +158,21 @@ function findGear(i, responseJson) {
         (itemList).push(
             "T-shirt", "Shorts", "Shoes", "Hats", "Sunglasses"
         )
-    } else {
-        console.log('gear not found')
     }
-
     return itemList;
 }
 
 function sortRenderGear(itemList) {
     let gearList = removeDuplicate(itemList);
-    console.log(gearList);
 
     for (i = 0; i < gearList.length; i++) {
         $('.js-gear-items').append(
             `<li>${gearList[i]}</li>`
         )
     };
-
 }
 
+// remove duplicate items from array
 function removeDuplicate(itemList) {
     let gearList = Array.from(new Set(itemList))
     return gearList
@@ -177,14 +186,17 @@ function nowClear() {
             $('form input[type=text]').val('');
             $('form input[type=date]').val('');
             $('.js-hi-lo, .js-gear-items').empty();
+            $('.js-error-msg').text('');
+            $('.js-error-msg').addClass('hidden');
+            $('#results').addClass('hidden');
         }
     })
-
 }
 
 function runProgram() {
     handleStart();
     watchForm();
+    nowClear();
 }
 
 $(runProgram);
